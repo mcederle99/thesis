9005b2e55da1806fed9375e5faafdfa54b55c834
matteo <matteita99@gmail.com>
2023-07-14 15:20:38 +0200

    boh

--- maddpg1_conv_2.py
+++ maddpg1_conv_2.py
@@ -1,5 +1,6 @@
 from util2 import make_env, plot_learning_curve
 
+import collections
 import torch as T
 import cv2 as cv
 import numpy as np
@@ -12,7 +13,7 @@ from smarts.zoo.agent_spec import AgentSpec
 from smarts.core.controllers import ActionSpaceType
 
 def obs_list_to_state_vector(observation, env):
-    stack = [collections.deque(maxlen=2)]
+    stack = collections.deque(maxlen=2)
 
     for i in range(stack.maxlen):
         stack.append(observation[i])
@@ -21,35 +22,31 @@ def obs_list_to_state_vector(observation, env):
     return state
 
 def map_action(action):
-    if action < -0.5:
+    if action < 0:
         discrete_action = 'keep_lane'
-    elif action < 0:
-        discrete_action = 'slow_down'
-    elif action < 0.5:
-        discrete_action = 'change_lane_left'
     else:
-        discrete_action = 'change_lane_right'
+        discrete_action = 'slow_down'
     return discrete_action
 
-def evaluate(agents, env, ep, step, n_eval=3):
+def evaluate(agents, env, ep, step, n_eval=10):
     score_history = []
     agent_007_starting_pos = 'edge-west-WE'
     obs = env.reset()
-    obs, _, _, infos = env.step({'Agent-007': 'slow_down', 'Agent-008': 'slow_down'})
+    obs, _, _, infos = env.step({'Agent-007': [0,0,0], 'Agent-008': [0,0,0]})
     agent_007_actual_pos = infos['Agent-007']['env_obs'][5].road_id
     while (len(obs.keys()) < 2) or (agent_007_starting_pos != agent_007_actual_pos):
         obs = env.reset()
-        obs, _, _, infos = env.step({'Agent-007': 'slow_down', 'Agent-008': 'slow_down'})
+        obs, _, _, infos = env.step({'Agent-007': [0,0,0], 'Agent-008': [0,0,0]})
         agent_007_actual_pos = infos['Agent-007']['env_obs'][5].road_id
 
         score = 0
         dones = [False] * 2
         while not any(dones):
-            actions = maddpg_agents.choose_action([obs['Agent-007'], obs['Agent-008']], evaluate=True)
-            discrete_action_1 = map_action(actions['Agent-007'])
-            discrete_action_2 = map_action(actions['Agent-008'])
-            agent_actions = {'Agent-007': discrete_action_1, 'Agent-008': discrete_action_2}
-            obs_, rewards, dones, infos = env.step(agent_actions)
+            actions = maddpg_agents.choose_action({'Agent-007': obs['Agent-007'], 'Agent-008': obs['Agent-008']}, evaluate=True)
+ #           discrete_action_1 = map_action(actions['Agent-007'])
+ #           discrete_action_2 = map_action(actions['Agent-008'])
+ #           agent_actions = {'Agent-007': discrete_action_1, 'Agent-008': discrete_action_2}
+            obs_, rewards, dones, infos = env.step(actions)
 
             list_done = list(dones.values())
             dones = list_done[:2]
@@ -67,7 +64,7 @@ if __name__ == '__main__':
     scenario = '3line_intersection_2agents'
     n_agents = 2
 
-    agent_spec = AgentSpec(interface=AgentInterface.from_type(AgentType.Laner, max_episode_steps=50))#, agent_builder=MADDPG)
+    agent_spec = AgentSpec(interface=AgentInterface.from_type(AgentType.Standard, max_episode_steps=50))#, agent_builder=MADDPG)
 
     agent_specs = {'Agent-007': agent_spec, 'Agent-008': agent_spec}
 
@@ -76,11 +73,10 @@ if __name__ == '__main__':
 
     actor_dims = [env.observation_space.shape, env.observation_space.shape]
     critic_dims = (env.observation_space.low.repeat(2,axis=0).shape)
-    #critic_dims = (3*n_agents, 84, 84)
 
-    n_actions = [1, 1]
-    maddpg_agents = MADDPG(actor_dims, critic_dims, n_agents, n_actions, alpha=0.0001, beta=0.001, env=scenario, gamma=0.95, chkpt_dir='results/maddpg1_conv/no_hd/')
-    memory = MultiAgentReplayBuffer(20000, critic_dims, actor_dims, n_actions, n_agents, batch_size=32)
+    n_actions = [3, 3]
+    maddpg_agents = MADDPG(actor_dims, critic_dims, n_agents, n_actions, alpha=0.0001, beta=0.001, env=scenario, gamma=0.95, chkpt_dir='results/maddpg1_conv/')
+    memory = MultiAgentReplayBuffer(50000, critic_dims, actor_dims, n_actions, n_agents, batch_size=64)
 
     EVAL_INTERVAL = 50
     MAX_STEPS = 1000000
@@ -99,21 +95,21 @@ if __name__ == '__main__':
 
     while total_steps < MAX_STEPS:
         obs = env.reset()
-        obs, _, _, infos = env.step({'Agent-007': 'slow_down', 'Agent-008': 'slow_down'})
+        obs, _, _, infos = env.step({'Agent-007': [0,0,0], 'Agent-008': [0,0,0]})
         agent_007_actual_pos = infos['Agent-007']['env_obs'][5].road_id
         while (len(obs.keys()) < 2) or (agent_007_starting_pos != agent_007_actual_pos):
             obs = env.reset()
-            obs, _, _, infos = env.step({'Agent-007': 'slow_down', 'Agent-008': 'slow_down'})
+            obs, _, _, infos = env.step({'Agent-007': [0,0,0], 'Agent-008': [0,0,0]})
             agent_007_actual_pos = infos['Agent-007']['env_obs'][5].road_id
 
         dones = [False]*2
         while not any(dones):
-            actions = maddpg_agents.choose_action([obs['Agent-007'], obs['Agent-008']])
-            discrete_action_1 = map_action(actions['Agent-007'])
-            discrete_action_2 = map_action(actions['Agent-008'])
-            agent_actions = {'Agent-007': discrete_action_1, 'Agent-008': discrete_action_2}
+            actions = maddpg_agents.choose_action({'Agent-007': obs['Agent-007'], 'Agent-008': obs['Agent-008']})
+#            discrete_action_1 = map_action(actions['Agent-007'])
+#            discrete_action_2 = map_action(actions['Agent-008'])
+#            agent_actions = {'Agent-007': discrete_action_1, 'Agent-008': discrete_action_2}
 
-            obs_, rewards, dones, infos = env.step(agent_actions)
+            obs_, rewards, dones, infos = env.step(actions)
 
             dones = list(dones.values())
             dones = dones[:2]
@@ -131,6 +127,7 @@ if __name__ == '__main__':
             obs = obs_
 
             total_steps += 1
+        episode += 1
         if episode % EVAL_INTERVAL == 0:
             score = evaluate(maddpg_agents, env, episode, total_steps)
             eval_scores.append(score)
@@ -138,3 +135,5 @@ if __name__ == '__main__':
             if score > best_score:
                 maddpg_agents.save_checkpoint()
                 best_score = score
+            np.save('maddpg_2_scores.npy', np.array(eval_scores))
+            np.save('maddpg_2_steps.npy', np.array(eval_steps))
