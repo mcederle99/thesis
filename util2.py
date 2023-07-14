e95538229076b169b3081fc4cba4abd9a11af5df
matteo <matteita99@gmail.com>
2023-07-14 15:47:01 +0200

    util

--- util2.py
+++ util2.py
@@ -29,6 +29,21 @@ def plot_learning_curve(x, scores, epsilons, filename):
 
     plt.savefig(filename)
 
+def plot_learning_curve_maddpg(x, scores, filename):
+    fig = plt.figure()
+    ax2 = fig.add_subplot(111, label=1)
+    running_avg = np.empty(N)
+    for t in range(N):
+        running_avg[t] = np.mean(scores[max(0, t-100):(t+1)])
+
+    ax2.scatter(x, running_avg, color='C1')
+    ax2.set_xlabel('Evaluation Steps')
+    ax2.set_ylabel('Score', color='C1')
+    ax2.tick_params(axis='x', colors='C0')
+    ax2.tick_params(axis='y', colors='C1')
+
+    plt.savefig(filename)
+
 class Reward(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
